01b89c5fb5026ac32f2fc3a68fdfadf40a8ab711
De Almeida Syméon <symeon.dealmeida@gmail.com>
2024-01-25 00:20:51 +1100

    Start the countact part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -114,6 +114,30 @@ export class AppComponent {
 
     // Use the typewriter
     this.typeWriterLoop();
+
+    // Get both parts of the countact logo
+    var countactLogoLeft = document.getElementById("CountactLeftSlider") as HTMLElement;
+    var countactLogoRight = document.getElementById("CountactRightSlider") as HTMLElement;
+
+    // Get both texts of the countact logo
+    var countactTextLeft = document.getElementById("CountactLeftText") as HTMLElement;
+    var countactTextRight = document.getElementById("CountactRightText") as HTMLElement;
+  
+    // Add event listeners to clicks
+    countactLogoLeft.addEventListener("click", function() {
+      if (!countactLogoLeft.classList.contains("hidden")) {
+        countactLogoLeft.classList.toggle("shown");
+        countactTextLeft.hidden = !countactTextLeft.hidden;
+        countactLogoRight.classList.toggle("hidden");
+      }
+    });
+    countactLogoRight.addEventListener("click", function() {
+      if (!countactLogoRight.classList.contains("hidden")) {
+        countactLogoLeft.classList.toggle("hidden");
+        countactLogoRight.classList.toggle("shown");
+        countactTextRight.hidden = !countactTextRight.hidden;
+      }
+    });
   }
 }
 
