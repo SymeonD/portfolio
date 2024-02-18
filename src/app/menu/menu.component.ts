7edd9ec862f22c2078a416b7c5aea5c561a83110
De Almeida Syméon <symeon.dealmeida@gmail.com>
2024-02-18 21:37:37 +1000

    Improve menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,4 +10,21 @@ import { CommonModule } from '@angular/common';
 })
 export class MenuComponent {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Get the links
+    const links = document.querySelectorAll('a');
+
+    // Add the click event
+    links.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const href = link.getAttribute('href')!;
+        const target = document.querySelector(href)!;
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      });
+    });
+  }
+
 }
