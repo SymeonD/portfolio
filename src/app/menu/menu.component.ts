7bcc26756df5a5b383755c7fb95fac06ba00d409
De Almeida Syméon <symeon.dealmeida@gmail.com>
2024-03-02 19:58:16 +1000

    Finish first page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -25,6 +25,17 @@ export class MenuComponent {
         target.scrollIntoView({ behavior: 'smooth', block: 'center' });
       });
     });
+
+    var prevScrollpos = window.scrollY;
+    window.onscroll = () => {
+      var currentScrollPos = window.scrollY;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("Menu")!.style.top = "0";
+      } else {
+        document.getElementById("Menu")!.style.top = "-80px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
   }
 
 }
